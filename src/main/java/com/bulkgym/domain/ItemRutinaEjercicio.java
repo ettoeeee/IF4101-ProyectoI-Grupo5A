a20c8a6bb7f8c282ce41604a9bc16b7d40b66be9
Jimena G <jimenaguevarach3@gmail.com>
2025-04-16 22:50:21 -0600

    Agregar la clase ItemRutinaEjercicio

--- src/main/java/com/bulkgym/domain/ItemRutinaEjercicio.java
+++ src/main/java/com/bulkgym/domain/ItemRutinaEjercicio.java
@@ -1,5 +1,55 @@
 package com.bulkgym.domain;
 
 public class ItemRutinaEjercicio {
-	
+    private int seriesEjercicio;
+    private int repeticionesEjercicio;
+    
+   
+    private Ejercicio ejercicio;  
+    private Rutina rutina;       
+
+
+    public ItemRutinaEjercicio() {
+    }
+
+
+    public ItemRutinaEjercicio(int seriesEjercicio, int repeticionesEjercicio, Ejercicio ejercicio, Rutina rutina) {
+        this.seriesEjercicio = seriesEjercicio;
+        this.repeticionesEjercicio = repeticionesEjercicio;
+        this.ejercicio = ejercicio;
+        this.rutina = rutina;
+    }
+
+
+    public int getSeriesEjercicio() {
+        return seriesEjercicio;
+    }
+
+    public void setSeriesEjercicio(int seriesEjercicio) {
+        this.seriesEjercicio = seriesEjercicio;
+    }
+
+    public int getRepeticionesEjercicio() {
+        return repeticionesEjercicio;
+    }
+
+    public void setRepeticionesEjercicio(int repeticionesEjercicio) {
+        this.repeticionesEjercicio = repeticionesEjercicio;
+    }
+
+    public Ejercicio getEjercicio() {
+        return ejercicio;
+    }
+
+    public void setEjercicio(Ejercicio ejercicio) {
+        this.ejercicio = ejercicio;
+    }
+
+    public Rutina getRutina() {
+        return rutina;
+    }
+
+    public void setRutina(Rutina rutina) {
+        this.rutina = rutina;
+    }
 }
