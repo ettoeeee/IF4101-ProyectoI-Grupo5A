cec0f65290c2a8b2a1c32b90aecfee0d5d874b5b
DVDCLVGMB <davidcalvogam@gmail.com>
2025-04-25 21:58:58 -0600

    Permisos globales para CORS en SecurityConfig.

--- src/main/java/com/bulkgym/restcontroller/ReporteRutinaRestController.java
+++ src/main/java/com/bulkgym/restcontroller/ReporteRutinaRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reportes")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ReporteRutinaRestController {
 
     @Autowired
