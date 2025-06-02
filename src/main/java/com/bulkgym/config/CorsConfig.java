4981b8d10b42f4133a0d2154278bea36b30545a9
Katherine <kfbrenes14@gmail.com>
2025-06-02 11:17:54 -0600

    Prueba conexión con Vercel funcional

--- src/main/java/com/bulkgym/config/CorsConfig.java
+++ src/main/java/com/bulkgym/config/CorsConfig.java
@@ -14,14 +14,18 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200",
+            "https://grupo5frontend.vercel.app"
+        ));
+        
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", config); //
+        source.registerCorsConfiguration("/api/**", config);
         return source;
     }
 }
-
